a66ea6b2f60cba42712864b4a2a2e5f635900e7b
_thread90 <39356292+thread90@users.noreply.github.com>
2021-01-30 08:44:32 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const browsers = ["Chrome", "IE", "Edge", "Firefox", "Safari"];
+const {Builder, By, Key, util} = require('selenium-webdriver');
+let driver;
+
+const browser = async(select="chrome", key="") => {
+    select = select.toLowerCase();
+    if(select == "firefox") {
+        driver = await new Builder().forBrowser('firefox').build();
+    } else if(select == "safari") {
+        driver = await new Builder().forBrowser('safari').build();
+    } else if(select == "edge") {
+        driver = await new Builder().forBrowser('edge').build();
+    } else if(select == "ie" || select == "internetexplorer" || select == "internet explorer") {
+        driver = await new Builder().forBrowser('ie').build();
+    } else if(select == "chrome") {
+        driver = await new Builder().forBrowser('chrome').build();
+    } else {
+        console.error("You must choose from this list for your browser: " + browsers.join(', '));
+        return;
+    }
+}
+
+const gotoWebsite = async(website="https://google.com") => {
+    await driver.get(website);
+}
+
+const get = async(element="q", sendKeys=false, send=["SimpleScraper",Key.RETURN]) => {
+    await driver.findElement(By.name(element)).sendKeys(send);
+}
+
+module.exports = {
+    browser, gotoWebsite, get
+}
---
+++ test/test.js
@@ -0,0 +1,3 @@
+const SimpleScraper = require('../index');
+
+SimpleScraper.browser('chrome');
